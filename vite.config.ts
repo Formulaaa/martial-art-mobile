68ddb59c2ea7598391315913fa528819c356c5bb
Formula <polaries111@163.com>
2023-02-05 09:40:32 +0800

    config: 增加按需引入配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import { createStyleImportPlugin, AndDesignVueResolve } from 'vite-plugin-style-import'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()]
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia']
+    }),
+    createStyleImportPlugin({
+      resolves: [AndDesignVueResolve()]
+    })
+  ],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, 'src')
     }
   },
   base: './',
