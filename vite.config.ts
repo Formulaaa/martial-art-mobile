21429d386302d3bdab1e15186d22a4b6cbc6f2f6
Formula <polaries111@163.com>
2023-02-06 16:50:22 +0800

    🔧工具(custom): 增加依赖：less，ant-design-vue，mock，vite-plugin-mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,60 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import { VantResolver } from 'unplugin-vue-components/resolvers'
+import { VantResolver, AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
-import { createStyleImportPlugin } from 'vite-plugin-style-import'
+import { createStyleImportPlugin, AndDesignVueResolve } from 'vite-plugin-style-import'
 import path from 'path'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [VantResolver()]
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
-      eslintrc: {
-        enabled: false, // Default `false`
-        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [VantResolver(), AntDesignVueResolver()]
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+        eslintrc: {
+          enabled: false, // Default `false`
+          filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+          globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+        }
+      }),
+      createStyleImportPlugin({
+        resolves: [AndDesignVueResolve()]
+      }),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: command === 'serve'
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          /**如果引入多个文件，可以使用
+           * '@import "@/assets/scss/globalVariable1.scss";
+           * @import"@/assets/scss/globalVariable2.scss";'
+           **/
+          additionalData: '@import "@/style/globalVar.scss";'
+        },
+        less:{
+          javascriptEnabled: true
+        }
       }
-    }),
-    createStyleImportPlugin({
-      resolves: []
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        /**如果引入多个文件，可以使用
-       * '@import "@/assets/scss/globalVariable1.scss";
-       * @import"@/assets/scss/globalVariable2.scss";'
-      **/
-        additionalData: '@import "@/style/globalVar.scss";',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
       }
+    },
+    base: './',
+    server: {
+      port: 3000,
+      open: true,
+      cors: true
     }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  base: './',
-  server: {
-    port: 3000,
-    open: true,
-    cors: true
   }
 })
